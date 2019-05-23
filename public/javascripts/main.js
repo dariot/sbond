ec25ce244c4a9514d27eebb81e608f9c0567d574
dariot <dario.turchetto@gmail.com>
2019-05-23 11:40:56 +0200

    using web3js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,40 @@
 $(document).ready(function() {
-    console.log('ready');
     $('#deploy').click(function(e) {
         e.preventDefault();
-        console.log('a');
+        var type = $('#types').val();
+        var description = $('#description').val();
+        var issuerSpread = $('#issuerSpread').val();
+        var valuationDate = $('#valuationDate').val();
+        var marketSpread = $('#marketSpread').val();
+        var frequency = $('#frequency').val();
+        var notional = $('#notional').val();
+        var indexation = $('#indexation').val();
+        var coupon = $('#coupon').val();
+        var convention = $('#convention').val();
+        var maturity = $('#maturity').val();
+        var bond = {
+            'type': type,
+            'description': description,
+            'issuerSpread': issuerSpread,
+            'valuationDate': valuationDate,
+            'marketSpread': marketSpread,
+            'frequency': frequency,
+            'notional': notional,
+            'indexation': indexation,
+            'coupon': coupon,
+            'convention': convention,
+            'maturity': maturity
+        };
+        
+        $.ajax({
+          type: 'POST',
+          data: {
+              prova: 'prova'
+          },
+          url: '/',
+          dataType: 'JSON'
+        }).done(function( response ) {
+            console.log(response);
+        });
     });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Web3 = require('web3');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Smart Bond' });
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req);
 });
 
 module.exports = router;
