77f78f4c14ba307f263d7670962d8b2ed6741e30
dariot <dario.turchetto@gmail.com>
2019-07-19 18:43:22 +0200

    changes

---
+++ contracts/SmartBond_v4.sol
@@ -0,0 +1,410 @@
+pragma solidity ^0.5.1;
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+  event OwnershipRenounced(address indexed previousOwner);
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+
+  /**
+   * Di default imposto come owner l'indirizzo creatore dello SmartContract 
+   */
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * Verifica che il chiamante sia l'owner dello SmartContract
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to relinquish control of the contract.
+   */
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipRenounced(owner);
+    owner = address(0);
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address _newOwner) public onlyOwner {
+    _transferOwnership(_newOwner);
+  }
+
+  /**
+   * @dev Transfers control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function _transferOwnership(address _newOwner) internal {
+    require(_newOwner != address(0));
+    emit OwnershipTransferred(owner, _newOwner);
+    owner = _newOwner;
+  }
+}
+
+library FixidityLib {
+
+    /**
+     * Indico il numero di cifre dedicate alla parte decimale dei numeri float (52,24).
+     */
+    function digits() public pure returns(uint8) {
+        return 24;
+    }
+    
+    /**
+     * Rappresenta il numero 1 in libreria fixato con le 24 cifre decimali (come dire 1, 24 zeri)
+     */
+    function fixed1() public pure returns(uint256) {
+        return 1000000000000000000000000;
+    }
+
+    /**
+     * @notice The amount of decimals lost on each multiplication operand.
+     * @dev Test mulPrecision() equals sqrt(fixed1)
+     * Hardcoded to 24 digits.
+     */
+    function mulPrecision() public pure returns(uint256) {
+        return 1000000000000;
+    }
+
+    /**
+     * Il numero intero più grande rappresentabile con un valore int256: ((2^255)-1)
+     */
+    function maxUint256() public pure returns(uint256) {
+        return 57896044618658097711785492504343953926634992332820282019728792003956564819967;
+    }
+
+    /**
+     * Il numero intero più grande che può essere convertito in tipo fixed point
+     * Test maxNewFixed() equals maxInt256() / fixed1()
+     * Hardcoded to 24 digits.
+     */
+    function maxNewFixed() public pure returns(uint256) {
+        return 57896044618658097711785492504343953926634992332820282;
+    }
+
+    /**
+     * Il numero intero più grande che può essere utilizzato in una addizione
+     * @dev Test maxFixedAdd() equals maxInt256()-1 / 2
+     * Test add(maxFixedAdd(),maxFixedAdd()) equals maxFixedAdd() + maxFixedAdd()
+     * Test add(maxFixedAdd()+1,maxFixedAdd()) throws 
+     * Test add(-maxFixedAdd(),-maxFixedAdd()) equals -maxFixedAdd() - maxFixedAdd()
+     * Test add(-maxFixedAdd(),-maxFixedAdd()-1) throws 
+     */
+    function maxFixedAdd() public pure returns(uint256) {
+        return 28948022309329048855892746252171976963317496166410141009864396001978282409983;
+    }
+
+    /**
+     * Il numero intero più grande che può essere utilizzato in una sottrazione
+     * @dev Test maxFixedSub() equals minInt256() / 2
+     */
+    function maxFixedSub() public pure returns(uint256) {
+        return 28948022309329048855892746252171976963317496166410141009864396001978282409984;
+    }
+
+    /**
+     * Il numero intero più grande che può essere utilizzato in una moltiplicazione
+     * @dev Calculated as sqrt(maxInt256()*fixed1()). 
+     * Be careful with your sqrt() implementation. I couldn't find a calculator
+     * that would give the exact square root of maxInt256*fixed1 so this number
+     * is below the real number by no more than 3*10**28. It is safe to use as
+     * a limit for your multiplications, although powers of two of numbers over
+     * this value might still work.
+     * Test multiply(maxFixedMul(),maxFixedMul()) equals maxFixedMul() * maxFixedMul()
+     * Test multiply(maxFixedMul(),maxFixedMul()+1) throws 
+     * Test multiply(-maxFixedMul(),maxFixedMul()) equals -maxFixedMul() * maxFixedMul()
+     * Test multiply(-maxFixedMul(),maxFixedMul()+1) throws 
+     * Hardcoded to 24 digits.
+     */
+    function maxFixedMul() public pure returns(uint256) {
+        return 240615969168004498257251713877715648331380787511296;
+    }
+
+    /**
+     * Il numero intero più grande che può essere utilizzato in una divisione come dividendo
+     * @dev divide(maxFixedDiv,newFixedFraction(1,fixed1())) = maxInt256().
+     * Test maxFixedDiv() equals maxInt256()/fixed1()
+     * Test divide(maxFixedDiv(),multiply(mulPrecision(),mulPrecision())) = maxFixedDiv()*(10^digits())
+     * Test divide(maxFixedDiv()+1,multiply(mulPrecision(),mulPrecision())) throws
+     * Hardcoded to 24 digits.
+     */
+    function maxFixedDiv() public pure returns(uint256) {
+        return 57896044618658097711785492504343953926634992332820282;
+    }
+
+    /**
+     * Il numero intero più grande che può essere utilizzato in una divisione come divisore
+     * @dev Test maxFixedDivisor() equals fixed1()*fixed1() - Or 10**(digits()*2)
+     * Test divide(10**(digits()*2 + 1),10**(digits()*2)) = returns 10*fixed1()
+     * Test divide(10**(digits()*2 + 1),10**(digits()*2 + 1)) = throws
+     * Hardcoded to 24 digits.
+     */
+    function maxFixedDivisor() public pure returns(uint256) {
+        return 1000000000000000000000000000000000000000000000000;
+    }
+
+    /**
+     * Converte un uint256 in un fixed point a 24 decimali
+     * @dev Test newFixed(0) returns 0
+     * Test newFixed(1) returns fixed1()
+     * Test newFixed(maxNewFixed()) returns maxNewFixed() * fixed1()
+     * Test newFixed(maxNewFixed()+1) fails
+     */
+    function newFixed(uint256 x) public pure returns (uint256) {
+        assert(x <= maxNewFixed());
+        //assert(x >= minNewFixed());
+        return x * fixed1();
+    }
+
+    /**
+     * Converte un fixed point a 24 decimali in un uint256 
+     * library to a non decimal. All decimal digits will be truncated.
+     */
+    function fromFixed(uint256 x) public pure returns (uint256) {
+        return x / fixed1();
+    }
+
+    /**
+     * @notice Converts two int256 representing a fraction to fixed point units,
+     * equivalent to multiplying dividend and divisor by 10^digits().
+     * @dev 
+     * Test newFixedFraction(maxFixedDiv()+1,1) fails
+     * Test newFixedFraction(1,maxFixedDiv()+1) fails
+     * Test newFixedFraction(1,0) fails     
+     * Test newFixedFraction(0,1) returns 0
+     * Test newFixedFraction(1,1) returns fixed1()
+     * Test newFixedFraction(maxFixedDiv(),1) returns maxFixedDiv()*fixed1()
+     * Test newFixedFraction(1,fixed1()) returns 1
+     * Test newFixedFraction(1,fixed1()-1) returns 0
+     */
+    function newFixedFraction(uint256 numerator, uint256 denominator) public pure returns (uint256) {
+        assert(numerator <= maxNewFixed());
+        assert(denominator <= maxNewFixed());
+        assert(denominator != 0);
+        uint256 convertedNumerator = newFixed(numerator);
+        uint256 convertedDenominator = newFixed(denominator);
+        return divide(convertedNumerator, convertedDenominator);
+    }
+
+    /**
+     * Torna la parte INTERA di un fixed point.
+     * Test integer(0) returns 0
+     * Test integer(fixed1()) returns fixed1()
+     * Test integer(newFixed(maxNewFixed())) returns maxNewFixed()*fixed1()
+     * Test integer(-fixed1()) returns -fixed1()
+     * Test integer(newFixed(-maxNewFixed())) returns -maxNewFixed()*fixed1()
+     */
+    function integer(uint256 x) public pure returns (uint256) {
+        return (x / fixed1()) * fixed1(); // Can't overflow
+    }
+
+    /**
+     * Torna la parte DECIMALE di un fixed point 
+     * In the case of a negative number the fractional is also negative.
+     * @dev 
+     * Test fractional(0) returns 0
+     * Test fractional(fixed1()) returns 0
+     * Test fractional(fixed1()-1) returns 10^24-1
+     * Test fractional(-fixed1()) returns 0
+     * Test fractional(-fixed1()+1) returns -10^24-1
+     */
+    function fractional(uint256 x) public pure returns (uint256) {
+        return x - (x / fixed1()) * fixed1(); // Can't overflow
+    }
+
+    /**
+     * @notice x+y. If any operator is higher than maxFixedAdd() it 
+     * might overflow.
+     * In solidity maxInt256 + 1 = minInt256 and viceversa.
+     * @dev 
+     * Test add(maxFixedAdd(),maxFixedAdd()) returns maxInt256()-1
+     * Test add(maxFixedAdd()+1,maxFixedAdd()+1) fails
+     * Test add(-maxFixedSub(),-maxFixedSub()) returns minInt256()
+     * Test add(-maxFixedSub()-1,-maxFixedSub()-1) fails
+     * Test add(maxInt256(),maxInt256()) fails
+     * Test add(minInt256(),minInt256()) fails
+     */
+    function add(uint256 x, uint256 y) public pure returns (uint256) {
+        uint256 z = x + y;
+        if (x > 0 && y > 0) assert(z > x && z > y);
+        if (x < 0 && y < 0) assert(z < x && z < y);
+        return z;
+    }
+
+    /**
+     * @notice x-y. You can use add(x,-y) instead. 
+     * @dev Tests covered by add(x,y)
+     */
+    function subtract(uint256 x, uint256 y) public pure returns (uint256) {
+        return add(x,-y);
+    }
+
+    /**
+     * @notice x*y. If any of the operators is higher than maxFixedMul() it 
+     * might overflow.
+     * @dev 
+     * Test multiply(0,0) returns 0
+     * Test multiply(maxFixedMul(),0) returns 0
+     * Test multiply(0,maxFixedMul()) returns 0
+     * Test multiply(maxFixedMul(),fixed1()) returns maxFixedMul()
+     * Test multiply(fixed1(),maxFixedMul()) returns maxFixedMul()
+     * Test all combinations of (2,-2), (2, 2.5), (2, -2.5) and (0.5, -0.5)
+     * Test multiply(fixed1()/mulPrecision(),fixed1()*mulPrecision())
+     * Test multiply(maxFixedMul()-1,maxFixedMul()) equals multiply(maxFixedMul(),maxFixedMul()-1)
+     * Test multiply(maxFixedMul(),maxFixedMul()) returns maxInt256() // Probably not to the last digits
+     * Test multiply(maxFixedMul()+1,maxFixedMul()) fails
+     * Test multiply(maxFixedMul(),maxFixedMul()+1) fails
+     */
+    function multiply(uint256 x, uint256 y) public pure returns (uint256) {
+        if (x == 0 || y == 0) return 0;
+        if (y == fixed1()) return x;
+        if (x == fixed1()) return y;
+
+        // Separate into integer and fractional parts
+        // x = x1 + x2, y = y1 + y2
+        uint256 x1 = integer(x) / fixed1();
+        uint256 x2 = fractional(x);
+        uint256 y1 = integer(y) / fixed1();
+        uint256 y2 = fractional(y);
+        
+        // (x1 + x2) * (y1 + y2) = (x1 * y1) + (x1 * y2) + (x2 * y1) + (x2 * y2)
+        uint256 x1y1 = x1 * y1;
+        if (x1 != 0) assert(x1y1 / x1 == y1); // Overflow x1y1
+        
+        // x1y1 needs to be multiplied back by fixed1
+        // solium-disable-next-line mixedcase
+        uint256 fixed_x1y1 = x1y1 * fixed1();
+        if (x1y1 != 0) assert(fixed_x1y1 / x1y1 == fixed1()); // Overflow x1y1 * fixed1
+        x1y1 = fixed_x1y1;
+
+        uint256 x2y1 = x2 * y1;
+        if (x2 != 0) assert(x2y1 / x2 == y1); // Overflow x2y1
+
+        uint256 x1y2 = x1 * y2;
+        if (x1 != 0) assert(x1y2 / x1 == y2); // Overflow x1y2
+
+        x2 = x2 / mulPrecision();
+        y2 = y2 / mulPrecision();
+        uint256 x2y2 = x2 * y2;
+        if (x2 != 0) assert(x2y2 / x2 == y2); // Overflow x2y2
+
+        // result = fixed1() * x1 * y1 + x1 * y2 + x2 * y1 + x2 * y2 / fixed1();
+        uint256 result = x1y1;
+        result = add(result, x2y1); // Add checks for overflow
+        result = add(result, x1y2); // Add checks for overflow
+        result = add(result, x2y2); // Add checks for overflow
+        return result;
+    }
+    
+    /**
+     * @notice 1/x
+     * @dev 
+     * Test reciprocal(0) fails
+     * Test reciprocal(fixed1()) returns fixed1()
+     * Test reciprocal(fixed1()*fixed1()) returns 1 // Testing how the fractional is truncated
+     * Test reciprocal(2*fixed1()*fixed1()) returns 0 // Testing how the fractional is truncated
+     */
+    function reciprocal(uint256 x) public pure returns (uint256) {
+        assert(x != 0);
+        return (fixed1()*fixed1()) / x; // Can't overflow
+    }
+
+    /**
+     * @notice x/y. If the dividend is higher than maxFixedDiv() it 
+     * might overflow. You can use multiply(x,reciprocal(y)) instead.
+     * There is a loss of precision on division for the lower mulPrecision() decimals.
+     * @dev 
+     * Test divide(fixed1(),0) fails
+     * Test divide(maxFixedDiv(),1) = maxFixedDiv()*(10^digits())
+     * Test divide(maxFixedDiv()+1,1) throws
+     * Test divide(maxFixedDiv(),maxFixedDiv()) returns fixed1()
+     */
+    function divide(uint256 x, uint256 y) public pure returns (uint256) {
+        if (y == fixed1()) return x;
+        assert(y != 0);
+        assert(y <= maxFixedDivisor());
+        return multiply(x, reciprocal(y));
+    }
+}
+
+// Estendo il contratto Ownable (in questa modalità si crea sempre un indirizzo del contratto)
+contract SmartBond is Ownable {
+
+    address payable beneficiarioAddress;
+    address payable creatorAddress;
+    uint256 frequency;
+    uint256 cedoleToPay; // numero di cedole da pagare
+    uint256 cedolaValue; 
+    uint256 interestTotalValue;
+    
+    /** Al momento della creazione dello SmartBond assegno: 
+    * il beneficiario tramite address
+    * la frequenza di pagamento delle cedole (valore in mesi)
+    * il totale dell'interesse dovuto (valore in ETH)
+    */
+    constructor (address payable _beneficiarioAddress, uint256 _frequency, uint256 _interestTotalValue) public {
+        beneficiarioAddress = _beneficiarioAddress;
+        frequency = FixidityLib.newFixed(_frequency);
+        creatorAddress = msg.sender;
+        interestTotalValue = FixidityLib.newFixed(_interestTotalValue);
+        cedolaValue = calcCedolaValue(frequency, interestTotalValue);
+        cedoleToPay = FixidityLib.divide((FixidityLib.fixed1()*12), frequency);
+    }
+
+    /** Gestione bilancio ETH
+    * consente allo SmartBond di ricevere ETH solo dall'owner
+    * il totale ricevuto dovrà essere la somma dell'interesse + il capitale nominale versato dal cliente
+    */
+    function receiveEth() public payable onlyOwner {
+        require(msg.value > 0);
+    }
+    
+    function payCedola() public onlyOwner {
+        if(cedoleToPay > FixidityLib.fixed1()){
+            sendAmountToBeneficiario(uint256(FixidityLib.fromFixed(cedolaValue)));
+            FixidityLib.subtract(cedoleToPay, FixidityLib.fixed1());
+        }
+        if(cedoleToPay == FixidityLib.fixed1()){
+            sendAmountToBeneficiario(address(this).balance);
+            FixidityLib.subtract(cedoleToPay, FixidityLib.fixed1());
+            killSmartBond();
+        }
+    }
+    
+    // Torna il bilancio di ETH presenti sullo SmartBond
+    function getEthBalance() public view onlyOwner returns(uint) {
+        return address(this).balance;
+    }
+    
+    // Termina la validità dello SmartBond e restituisce il balance al creatorAddress se > 0
+    function killSmartBond() public onlyOwner {
+        selfdestruct(creatorAddress);
+    }
+    
+    // Determino il valore della singola cedola (in valori fixed point)
+    function calcCedolaValue(uint256 _frequency, uint256 _interestValue) private returns(uint256) {
+        cedolaValue = FixidityLib.divide(_interestValue, _frequency);
+    }
+    
+    // Invio ETH al beneficiario
+    function sendAmountToBeneficiario(uint256 _payAmount) private {
+        require(_payAmount > 0 && address(this).balance >= _payAmount);
+        beneficiarioAddress.transfer(_payAmount);
+    }
+}
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,49 @@
 $(document).ready(function() {
+    
+    function showFixedRBFields() {
+        $('#valuationDateFG').show();
+        $('#frequencyFG').show();
+        $('#faceValueFG').show();
+        $('#couponFG').show();
+        $('#conventionFG').show();
+        $('#maturityFG').show();
+    }
+    
+    function showFRNFields() {
+        $('#descriptionFG').show();
+        $('#issuerSpreadFG').show();
+        $('#valuationDateFG').show();
+        $('#marketSpreadFG').show();
+        $('#frequencyFG').show();
+        $('#notionalFG').show();
+        $('#indexationFG').show();
+        $('#couponFG').show();
+        $('#conventionFG').show();
+        $('#maturityFG').show();
+    }
+    
+    function hideFields() {
+        $('#descriptionFG').hide();
+        $('#issuerSpreadFG').hide();
+        $('#valuationDateFG').hide();
+        $('#faceValueFG').hide();
+        $('#marketSpreadFG').hide();
+        $('#frequencyFG').hide();
+        $('#notionalFG').hide();
+        $('#indexationFG').hide();
+        $('#couponFG').hide();
+        $('#conventionFG').hide();
+        $('#maturityFG').hide();
+    }    
+    
     $('#deploy').click(function(e) {
         e.preventDefault();
         var type = $('#types').val();
         var description = $('#description').val();
         var issuerSpread = $('#issuerSpread').val();
         var valuationDate = $('#valuationDate').val();
+        var frequency = $('#frequency').val();
+        var faceValue = $('#faceValue').val();
         var marketSpread = $('#marketSpread').val();
         var frequency = $('#frequency').val();
         var notional = $('#notional').val();
@@ -12,11 +51,14 @@ $(document).ready(function() {
         var coupon = $('#coupon').val();
         var convention = $('#convention').val();
         var maturity = $('#maturity').val();
+        
         var bond = {
             'type': type,
             'description': description,
             'issuerSpread': issuerSpread,
             'valuationDate': valuationDate,
+            'frequency': frequency,
+            'faceValue': faceValue,
             'marketSpread': marketSpread,
             'frequency': frequency,
             'notional': notional,
@@ -37,4 +79,27 @@ $(document).ready(function() {
             console.log(response);
         });
     });
+    
+    function setFieldsVisibility(type) {
+        hideFields();
+        if (type === 'FixedRB') {
+            showFixedRBFields();
+        } else if (type === 'FRN') {
+            showFRNFields();
+        }
+    }
+    
+    $('#types').change(function() {
+        var type = $('#types').val();
+        setFieldsVisibility(type);
+    });
+    
+    hideFields();
+    var type = $('#types').val();
+    if (type === 'FixedRB') {
+        showFixedRBFields();
+    } else if (type === 'FRN') {
+        showFRNFields();
+    }
+    
 });
