7bb393c8d4bedc139c6a774d8ca4cb2c9b50d3a8
dariot <dario.turchetto@gmail.com>
2019-11-17 20:12:59 +0100

    login form

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,19 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-    console.log(req.body);
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    var mongodbOptions = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    };
+    mongoose.connect('mongodb://localhost:27017/test', mongodbOptions);
+    var db = mongoose.connection;
+    db.on('error', console.error.bind('connection error:'));
+    db.once('open', function() {
+        /* todo */
+    });
 });
 
 module.exports = router;
